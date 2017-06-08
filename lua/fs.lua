68144c8d94d7d077d2864cb36c7d7853dfd620cf
yanxurui <617080352@qq.com>
2017-06-08 14:14:56 +0800

    use private fields

--- lua/fs.lua
+++ lua/fs.lua
@@ -2,50 +2,28 @@ require 'util'
 local config = require 'config'
 local redish = require 'redis_helper'
 
-local _M = {
-    -- write_point = {f, volume_id, offset}
-    fset={}
-    -- fset = {
-    --  volume_id1 = f1,
-    --  volume_id2 = f2,
-    --  ...
-    -- }
-}
 
+local _M = {}
+
+
+local write_point = nil
+-- write_point = {f, volume_id, offset, erase_offset}
+local fset={}
+-- fset = {
+--  volume_id1 = f1,
+--  volume_id2 = f2,
+--  ...
+-- }
 local volume_num = math.floor(config.max_capacity/config.volume_size)
 
-local function update_write_point(volume_id, offset)
-    -- save in module
-    _M.write_point = {
-        ['volume_id'] = volume_id,
-        ['offset'] = offset
-    }
 
-    -- open file
-    local path = config.mount_point..'/'..volume_id
-    -- update mode, all previous data is preserved, in binary mode
-    local f = io.open(path, 'rb+')
-    if not f then
-        ngx.log(ngx.ERR, "failed to open file: ", path)
-        -- todo
-        return
-    end
-    local pos, err = f:seek("set", offset)
-    if not pos then
-        ngx.log(ngx.ERR, 'failed to seek: ', err)
-        return
-    end
-
-    _M.write_point['f'] = f
-end
-
-local function get_write_point()
+local function update_write_point()
     local volume_id, offset
-    if not _M.write_point then
+    if not write_point then
         local code, res = redish.get(config.mount_point)
         if code == 200 then
-            local write_point = split(res, ',')
-            volume_id, offset = tonumber(write_point[1]), tonumber(write_point[2])
+            local temp = split(res, ',')
+            volume_id, offset = tonumber(temp[1]), tonumber(temp[2])
         elseif code == 404 then
             ngx.log(ngx.WARN, 'restore write point not found in redis')
             volume_id, offset = 0, 0
@@ -53,25 +31,44 @@ local function get_write_point()
             ngx.log(ngx.ERR, 'failed to restore write point from redis: ', res)
             return
         end
-        update_write_point(volume_id, offset)
     else
-        local old_volume_id = _M.write_point['volume_id']
         -- if the size of the current file is bigger than limit
-        -- then open another file for write
-        if _M.write_point['offset'] > config.volume_size then
-            volume_id = (old_volume_id+1)%volume_num
-            if volume_id ~= old_volume_id then
-                ngx.log(ngx.INFO, 'rotate to volume ', volume_id)
-            end
-            _M.write_point['volume_id'] = volume_id
-            _M.write_point['offset'] = 0
+        -- then open another file to write
+        if write_point.offset > config.volume_size then
+            volume_id = (write_point.volume_id+1)%volume_num
+            ngx.log(ngx.INFO, 'rotate to volume ', volume_id)
+            write_point.f:close()
             offset = 0
-            _M.write_point['f']:close()
-            update_write_point(volume_id, offset)
+        else
+            return
         end
     end
 
-    return _M.write_point
+    -- save in module
+    write_point = {
+        volume_id = volume_id,
+        offset = offset
+    }
+
+    -- open file
+    local path = config.mount_point..'/'..volume_id
+    -- update mode, all previous data is preserved, in binary mode
+    local f = io.open(path, 'rb+')
+    if not f then
+        -- maybe not exist
+        f = io.open(path, 'wb')
+        if not f then
+            ngx.log(ngx.ERR, "failed to open file: ", path)
+            return
+        end
+    end
+    local pos, err = f:seek("set", offset)
+    if not pos then
+        ngx.log(ngx.ERR, 'failed to seek: ', err)
+        return
+    end
+
+    write_point.f = f
 end
 
 function _M.write(key, data)
@@ -86,12 +83,9 @@ function _M.write(key, data)
     end
 
     -- get write point
-    local write_point = get_write_point()
-    local volume_id=write_point['volume_id']
-    local offset=write_point['offset']
-    local f=write_point['f']
+    update_write_point()
     -- write to file
-    local ok, err = f:write(data)
+    local ok, err = write_point.f:write(data)
     if not ok then
         ngx.log(ngx.ERR, 'failed to write: ', err)
         return 500
@@ -100,22 +94,23 @@ function _M.write(key, data)
     -- write out any data that lingers in a program buffer to the actual file. 
     -- Specifically what this means is that if another process has that same file open for reading, 
     -- it will be able to access the data you just flushed to the file. However, it does not necessarily mean it has been "permanently" stored on disk.
-    f:flush()
+    write_point.f:flush()
 
     local length = #data
-    -- update write point
-    write_point['offset'] = offset+length
+    -- update offset
+    local offset=write_point.offset
+    write_point.offset = offset+length
     -- save index
     -- todo
-    local status, res = redish.set(key, table.concat({volume_id, offset, length}, ','))
+    local status, res = redish.set(key, table.concat({write_point.volume_id, offset, length}, ','))
     if status ~= 200 then
         return status, res
     end
-    return redish.set(config.mount_point, table.concat({volume_id, write_point['offset']}, ','))
+    return redish.set(config.mount_point, table.concat({write_point.volume_id, write_point.offset}, ','))
 end
 
 local function get_file(volume_id)
-    local f = _M.fset[volume_id]
+    local f = fset[volume_id]
     if not f then
         local path = config.mount_point..'/'..volume_id
         f = io.open(path, 'rb')
@@ -124,7 +119,7 @@ local function get_file(volume_id)
             -- todo
             return
         end
-        _M.fset[volume_id] = f
+        fset[volume_id] = f
     end
     return f
 end
@@ -170,12 +165,6 @@ function _M.delete(key)
     return redish:del(key)
 end
 
--- cannot read from file immediately after write without flush
-function _M.flush()
-    if _M.write_point then
-        _M.write_point['f']:flush()
-    end
-end
 
 return _M
 
